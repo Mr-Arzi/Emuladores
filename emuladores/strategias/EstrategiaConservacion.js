0c61bf647a50f05facca6ac34ca1e4523ba7932e
Mr-Arzi <goop.robecarlos@gmail.com>
2026-05-28 22:16:01 -0600

    pruebas de emuladores

--- emuladores/strategias/EstrategiaConservacion.js
+++
@@ -1,8 +0,0 @@
-// estrategias/EstrategiaConservacion.js
-class EstrategiaConservacion {
-    evaluar(datosSensor) {
-        throw new Error("El método evaluar() debe ser implementado");
-    }
-}
-
-module.exports = EstrategiaConservacion;
